2e62188cd4bfcc49ba48d924c7d982f5c806cf9f
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-09-14 19:10:55 +0530

    Add Full website with links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
-class App extends Calculator {
-  render() {
-    return (
-      <Calculator />
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
